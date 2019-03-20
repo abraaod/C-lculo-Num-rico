20dbea4a7fe13b580271c4156268c8981eaeb34b
Confuzo <abraaovld@gmail.com>
2019-03-20 15:18:22 -0300

    Main pronto

--- lista3/src/main.cpp
+++ lista3/src/main.cpp
@@ -2,6 +2,9 @@
 int main(int argc, char const *argv[]){
 
     Sistema sistema(argv[1]);
+    sistema.solucionar();
+    std::cout << "A matriz triangularizada: \n";
     sistema.imprimir();
+    sistema.printresultados();
     
 }
